fcb48fe8b6dc3509d31de87806375a490b8afb12
binh-t-nguyen <50223271+binh-t-nguyen@users.noreply.github.com>
2020-03-21 09:38:12 -0400

    Update dockerhub user

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('bndocker/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('bndocker')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
